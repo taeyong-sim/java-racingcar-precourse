47def6c0fdd08c184228d386b9308f9b8d96aa8e
taeyong-sim <sim2ty@naver.com>
2022-04-24 02:08:37 +0900

    refactor(RacingController) : Controller, Service 기능분리를 위한 로직 변경

---
+++ src/main/java/racingcar/controller/RacingController.java
@@ -0,0 +1,76 @@
+package racingcar.controller;
+
+import racingcar.common.InfoMessage;
+import racingcar.domain.Cars;
+import racingcar.domain.Game;
+import racingcar.domain.Round;
+import racingcar.service.ResultService;
+import racingcar.service.GameService;
+import racingcar.service.InputService;
+import racingcar.ui.Input;
+import racingcar.ui.Output;
+
+public class RacingController {
+
+    private Input input;
+    private ResultService resultService;
+    private InputService inputService;
+    private GameService gameService;
+    private Cars cars;
+    private Game game;
+
+    public void run() {
+        initCarService();
+        inputToStartGame();
+        processGame();
+        showResult();
+    }
+
+    private void initCarService() {
+        resultService = new ResultService();
+    }
+
+    private void inputToStartGame() {
+        cars = inputCarName();
+        inputService = new InputService(cars);
+        game = inputRound();
+        gameService = new GameService(game);
+    }
+
+    private Cars inputCarName() {
+        Output.printMessage(InfoMessage.INPUT_CAR_NAME);
+        try {
+            input = new Input();
+            return new Cars(input.splitCarNames(input.getInput()));
+        } catch (IllegalArgumentException e) {
+            Output.printMessageWithLineSpacing(e.getMessage());
+            return inputCarName();
+        }
+    }
+
+    private Game inputRound() {
+        Output.printMessageWithLineSpacing(InfoMessage.INPUT_MOVEMENT_LIMIT);
+        try{
+            input = new Input();
+            return new Game(inputService.getCars(), new Round(input.getInput()));
+        }catch (IllegalArgumentException e){
+            Output.printMessageWithLineSpacing(e.getMessage());
+            return inputRound();
+        }
+    }
+
+    private void processGame() {
+        Output.printMessageWithLineSpacing(InfoMessage.OUTPUT_RUN_RESULT);
+        for (int i = 0; i < game.getRound().getRoundNum(); i++) {
+            gameService.playGame(game);
+            Output.printLineSpacing();
+        }
+    }
+
+    private void showResult() {
+        resultService.determineMaxPosition(game);
+        resultService.determineWinners(game);
+        Output.printWinners(resultService.getWinners());
+    }
+
+}
